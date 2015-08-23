624a1a8027e5b2481f337db2d1faa96e7e941825
FighterRay <554269861@qq.com>
2015-08-23 14:23:41 +0800

    Add Shop function by ZhangRunfeng

---
+++ MoblieCQUPT_iOS/MoblieCQUPT_iOS/Controller/Shop/ShopDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  ShopDetailViewController.h
+//  MoblieCQUPT_iOS
+//
+//  Created by 张润峰 on 15/8/19.
+//  Copyright (c) 2015年 Orange-W. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShopDetailViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIImageView *picture;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *addressLabel;
+
+@end
