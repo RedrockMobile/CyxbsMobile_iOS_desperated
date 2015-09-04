0922d3145928830cc795d243ad5e366dc733c41e
Orange-W <495871401@163.com>
2015-09-04 10:49:25 +0800

    suggest

---
+++ MoblieCQUPT_iOS/MoblieCQUPT_iOS/Controller/Suggestion/SuggestionViewController.h
@@ -0,0 +1,13 @@
+//
+//  SuggestionViewController.h
+//  MoblieCQUPT_iOS
+//
+//  Created by user on 15/9/3.
+//  Copyright (c) 2015年 Orange-W. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SuggestionViewController : UIViewController
+
+@end
--- MoblieCQUPT_iOS/MoblieCQUPT_iOS/main.m
+++ MoblieCQUPT_iOS/MoblieCQUPT_iOS/main.m
@@ -1,4 +1,4 @@
-;//
+//
 //  main.m
 //  MoblieCQUPT_iOS
 //
