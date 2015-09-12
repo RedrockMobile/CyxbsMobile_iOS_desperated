9f5b233710d22fbb62d73a2da6b98257f1210312
RainyTunes <cquptrainytunes@gmail.com>
2015-09-12 22:01:31 +0800

    pull前commit

---
+++ MoblieCQUPT_iOS/MoblieCQUPT_iOS/Controller/Consult/XBSFindClassroomViewController.m
@@ -0,0 +1,37 @@
+//
+//  XBSFindClassroomViewController.m
+//  MoblieCQUPT_iOS
+//
+//  Created by RainyTunes on 9/12/15.
+//  Copyright (c) 2015 Orange-W. All rights reserved.
+//
+
+#import "XBSFindClassroomViewController.h"
+
+@interface XBSFindClassroomViewController ()
+
+@end
+
+@implementation XBSFindClassroomViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
