fde60e67b628a78074af04b0be4da03075904e7f
SugarGirl <chenxinhong@gqueen-jiyeon.local>
2015-09-22 18:57:29 +0800

    cxh 9-22 18:57

--- MoblieCQUPT_iOS/MoblieCQUPT_iOS/Controller/Course/CourseView.m
+++ MoblieCQUPT_iOS/MoblieCQUPT_iOS/Controller/Course/CourseView.m
@@ -26,22 +26,26 @@
 
     NSArray *array1 = [[NSArray alloc]initWithObjects:@"名称",@"老师",@"教室",@"时间",@"属性",@"周数", nil];
     NSArray *array2 = [[NSArray alloc]initWithObjects:@"iconfont-wodekecheng.png",@"iconfont-menuiconaccount.png",@"iconfont-location.png",@"iconfont-clock.png",@"iconfont-status.png",@"iconfont-week.png", nil];
-    NSArray *array3 = [[NSArray alloc]initWithObjects:[dic objectForKey:@"course"],[dic objectForKey:@"teacher"],[dic objectForKey:@"classroom"],[NSString stringWithFormat:@"%@~%@",[dic objectForKey:@"day"],[dic objectForKey:@"lesson"]],[dic objectForKey:@"type"],[dic objectForKey:@"rawWeek"], nil];
+    NSArray *array3 = [[NSArray alloc]initWithObjects:[dic objectForKey:@"course"],[dic objectForKey:@"teacher"],[dic objectForKey:@"classroom"],[NSString stringWithFormat:@"%@",[dic objectForKey:@"lesson"]],[dic objectForKey:@"type"],[dic objectForKey:@"rawWeek"], nil];
     for (int i = 0; i < 6; i ++) {
         UIView *view = [[UIView alloc]initWithFrame:CGRectMake(0, kViewHeight*i, kViewWidth, kViewHeight)];
         [infoView addSubview:view];
         
         UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(view.frame.size.width/20, view.frame.size.height/2.5, view.frame.size.width/15, view.frame.size.width/15)];
+        imageView.center = CGPointMake(view.frame.size.width/15, view.frame.size.height/2);
         imageView.image = [UIImage imageNamed:array2[i]];
         [view addSubview:imageView];
         
         UILabel *label1 = [[UILabel alloc]initWithFrame:CGRectMake(imageView.frame.size.width+view.frame.size.width/25*2, view.frame.size.height/3, view.frame.size.width/10*1.5, view.frame.size.width/10)];
+        label1.center = CGPointMake(view.frame.size.width/10*2, view.frame.size.height/2);
         label1.text = array1[i];
-        label1.font = [UIFont systemFontOfSize:15];
+        label1.font = [UIFont systemFontOfSize:14];
         label1.textAlignment = NSTextAlignmentCenter;
         [view addSubview:label1];
         
         UILabel *label2 = [[UILabel alloc]initWithFrame:CGRectMake(label1.frame.size.width+view.frame.size.width/10*2, view.frame.size.height/3, view.frame.size.width/10*6, view.frame.size.width/10)];
+        label2.center = CGPointMake(view.frame.size.width/10*6, view.frame.size.height/2);
+        label2.font = [UIFont systemFontOfSize:16];
         label2.text = array3[i];
         [view addSubview:label2];
     }
