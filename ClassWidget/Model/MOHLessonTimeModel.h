79aed8d415f25a9441188597e9ff66aeb9051e14
user <495871401>
2016-03-18 22:41:36 +0800

    add model

---
+++ ClassWidget/Model/MOHLessonTimeModel.h
@@ -0,0 +1,13 @@
+//
+//  MOHLessonTimeModel.h
+//  MoblieCQUPT_iOS
+//
+//  Created by user on 16/3/18.
+//  Copyright © 2016年 Orange-W. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MOHLessonTimeModel : NSObject
+
+@end
---
+++ ClassWidget/Model/MOHLessonTimeModel.m
@@ -0,0 +1,13 @@
+//
+//  MOHLessonTimeModel.m
+//  MoblieCQUPT_iOS
+//
+//  Created by user on 16/3/18.
+//  Copyright © 2016年 Orange-W. All rights reserved.
+//
+
+#import "MOHLessonTimeModel.h"
+
+@implementation MOHLessonTimeModel
+
+@end
