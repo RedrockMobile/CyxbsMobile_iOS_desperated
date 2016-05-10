022d5134f3dfb5ee752e96b53b13fa03e5efbd99
SugarGirl <chengege1004@gmail.com>
2016-05-10 10:20:55 +0800

    解决返回data为null的问题 解决未完成信息 下次登录自动跳转到主界面的bug

---
+++ MoblieCQUPT_iOS/Controller/MBCommunity/Category/NSNull+Helper.m
@@ -0,0 +1,13 @@
+//
+//  NSNull+Helper.m
+//  MoblieCQUPT_iOS
+//
+//  Created by GQuEen on 16/5/10.
+//  Copyright © 2016年 Orange-W. All rights reserved.
+//
+
+#import "NSNull+Helper.h"
+
+@implementation NSNull (Helper)
+
+@end
