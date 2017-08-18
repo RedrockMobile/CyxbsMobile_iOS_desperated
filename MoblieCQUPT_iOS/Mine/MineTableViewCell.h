4655122bfd6b7f5532739e16fa8f4dedb9f83e0f
Johnlee666 <895471139@qq.com>
2017-08-18 15:33:02 +0800

    我的板块 UI改版

---
+++ MoblieCQUPT_iOS/Mine/MineTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MineTableViewCell.h
+//  MoblieCQUPT_iOS
+//
+//  Created by 李展 on 2017/8/18.
+//  Copyright © 2017年 Orange-W. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MineTableViewCell : UITableViewCell
+
+@end
---
+++ MoblieCQUPT_iOS/Mine/MineTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  MineTableViewCell.m
+//  MoblieCQUPT_iOS
+//
+//  Created by 李展 on 2017/8/18.
+//  Copyright © 2017年 Orange-W. All rights reserved.
+//
+
+#import "MineTableViewCell.h"
+
+@implementation MineTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
