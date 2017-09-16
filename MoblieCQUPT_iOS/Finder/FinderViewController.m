c7fe6a9fbeff58530098338ee5eab34bc7b5ece5
Johnlee666 <895471139@qq.com>
2017-09-16 21:21:53 +0800

    Find Banner

--- MoblieCQUPT_iOS/Finder/FinderViewController.m
+++ MoblieCQUPT_iOS/Finder/FinderViewController.m
@@ -14,7 +14,10 @@
 
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 @property (weak, nonatomic) IBOutlet FinderCollectionViewFlowLayout *layout;
+@property (strong, nonatomic) NSTimer *timer;
 @property NSArray *array;
+@property NSInteger selectedIndex;
+@property BOOL firstLoad;
 #define N 1000
 @end
 
@@ -22,23 +25,77 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.firstLoad = YES;
+    
     self.collectionView.delegate = self;
     self.collectionView.dataSource = self;
     self.collectionView.showsVerticalScrollIndicator = NO;
     self.collectionView.showsHorizontalScrollIndicator = NO;
+    self.collectionView.scrollEnabled = NO;
+    UISwipeGestureRecognizer *leftSwipeGesture = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(swipe:)];
+    leftSwipeGesture.direction = UISwipeGestureRecognizerDirectionLeft;
+    UISwipeGestureRecognizer *rightSwipeGesture = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(swipe:)];
+    rightSwipeGesture.direction = UISwipeGestureRecognizerDirectionRight;
+    [self.collectionView addGestureRecognizer:leftSwipeGesture];
+    [self.collectionView addGestureRecognizer:rightSwipeGesture];
+    // 产品的要求 一次滑动只能移动一个
+    
     [self.collectionView registerNib:[UINib nibWithNibName:@"FinderCollectionViewCell" bundle:nil]forCellWithReuseIdentifier:@"FinderCollectionViewCell"];
     self.array = @[@"cqupt1.jpg",@"cqupt2.jpg",@"cqupt3.jpg"];
-   
-//    [self.collectionView scrollToItemAtIndexPath:3*N/2 atScrollPosition:UICollectionViewScrollDirectionHorizontal animated:NO];
+    self.selectedIndex = self.array.count*N/2;
+    
 }
 
-- (void)viewDidLayoutSubviews {
+- (void)viewDidAppear:(BOOL)animated{
+    [self addTimer];
+}
+
+- (void)viewDidDisappear:(BOOL)animated{
+    [self removeTimer];
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"%ld",(long)indexPath.row);
+}
+
+-(void)addTimer{
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:5 target:self selector:@selector(changeImage) userInfo:nil repeats:YES];
+}
+
+- (void)changeImage{
+    self.selectedIndex = (++self.selectedIndex)%(self.array.count*N);
+    [self.collectionView scrollToItemAtIndexPath:[NSIndexPath indexPathForRow:self.selectedIndex inSection:0] atScrollPosition:UICollectionViewScrollPositionCenteredHorizontally animated:YES];
+}
+
+-(void)removeTimer{
+    [self.timer invalidate];
+    self.timer = nil;
+}
+
+
+- (void)viewDidLayoutSubviews{
     [super viewDidLayoutSubviews];
-    self.layout.itemSize = CGSizeMake(self.collectionView.width-100, self.collectionView.height);
-    [self.collectionView scrollToItemAtIndexPath:[NSIndexPath indexPathForRow:self.array.count*N/2 inSection:0] atScrollPosition:UICollectionViewScrollPositionCenteredHorizontally animated:NO];
-
+    if (self.firstLoad) {
+        self.layout.itemSize = CGSizeMake(self.collectionView.width-100, self.collectionView.height);
+        self.layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+        [self.collectionView scrollToItemAtIndexPath:[NSIndexPath indexPathForRow:self.selectedIndex inSection:0] atScrollPosition:UICollectionViewScrollPositionCenteredHorizontally animated:NO];
+        self.firstLoad = NO;
+    }
 }
 
+
+- (void)swipe:(UISwipeGestureRecognizer *)getsure{
+    [self removeTimer];
+    if (getsure.direction == UISwipeGestureRecognizerDirectionLeft) {
+        self.selectedIndex = (++self.selectedIndex)%(self.array.count*N);
+    }
+    else if(getsure.direction == UISwipeGestureRecognizerDirectionRight){
+        self.selectedIndex = (--self.selectedIndex)%(self.array.count*N);
+    }
+    [self.collectionView scrollToItemAtIndexPath:[NSIndexPath indexPathForRow:self.selectedIndex inSection:0] atScrollPosition:UICollectionViewScrollPositionCenteredHorizontally animated:YES];
+    [self addTimer];
+    
+}
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
     return self.array.count*N;
