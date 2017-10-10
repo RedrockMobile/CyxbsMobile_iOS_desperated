4971f29e7fdab5cf4db6cf8804e74861768ed469
LiangXiao <18016139255@163.com>
2017-10-10 21:01:03 +0800

    修改bug

--- MoblieCQUPT_iOS/Mine/MyMessages/MyMessagesViewController.m
+++ MoblieCQUPT_iOS/Mine/MyMessages/MyMessagesViewController.m
@@ -127,7 +127,11 @@
             [self.allDataArray addObject:viewModel];
         }
         [self.communityTableView reloadData];
-        [self.communityTableView.mj_footer endRefreshing];
+        if (dataArray.count < 15) {
+            [self.communityTableView.mj_footer endRefreshingWithNoMoreData];
+        } else {
+            [self.communityTableView.mj_footer endRefreshing];
+        }
         [self.communityTableView.mj_header endRefreshing];
     } WithFailureBlock:^{
         NSLog(@"请求失败");
