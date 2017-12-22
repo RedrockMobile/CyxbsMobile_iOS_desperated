c4c8efacf5cc84ab31edc8b99537b7e7fbd9b4ee
Johnlee666 <895471139@qq.com>
2017-12-22 17:10:51 +0800

    improve segmentView

--- MoblieCQUPT_iOS/Tool/SegmentView.h
+++ MoblieCQUPT_iOS/Tool/SegmentView.h
@@ -9,17 +9,18 @@
 #import <UIKit/UIKit.h>
 
 @protocol SegmentViewDelegate <NSObject>
-
 @required
-
 - (void)eventWhenScrollSubViewWithIndex:(NSInteger)index;
-
 @end
 
 @interface SegmentView : UIView
 
-@property (weak,nonatomic) id<SegmentViewDelegate> eventDelegate;
-
+@property (nonatomic, weak) id<SegmentViewDelegate> eventDelegate;
+@property (nonatomic, assign) CGFloat titleViewHeight;
+@property (nonatomic, strong) UIColor *selectedTitleColor;
+@property (nonatomic, strong) UIColor *titleColor;
+@property (nonatomic, assign, readonly) NSInteger currentIndex;
+@property (nonatomic, strong) UIFont *font;
 - (instancetype)initWithFrame:(CGRect)frame andControllers:(NSArray <UIViewController *> *)controllers;
 
 @end
