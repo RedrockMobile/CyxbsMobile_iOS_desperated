92e2f7e8a671682c0b1c9a4a8e232fbc5043dc0c
xiaogou134 <xiaogou134@hotmail.com>
2018-06-02 17:29:08 +0800

    提交一次看下梁萧

---
+++ MoblieCQUPT_iOS/Mine/drafts/draftsTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  draftsTableViewCell.h
+//  MoblieCQUPT_iOS
+//
+//  Created by xiaogou134 on 2018/6/2.
+//  Copyright © 2018年 Orange-W. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface draftsTableViewCell : UITableViewCell
++ (instancetype)cellWithTableView:(UITableView *)tableView AndLab:(BOOL)whether;
+@end
