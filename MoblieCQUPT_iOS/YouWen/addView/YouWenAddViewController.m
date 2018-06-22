db03798f100e33948b71a95859fa494b9f042652
xiaogou134 <xiaogou134@hotmail.com>
2018-06-22 10:30:19 +0800

    修改bug
    修改了添加话题的bug
    修改了收起键盘的bug

--- MoblieCQUPT_iOS/YouWen/addView/YouWenAddViewController.m
+++ MoblieCQUPT_iOS/YouWen/addView/YouWenAddViewController.m
@@ -94,18 +94,6 @@
     return YES;
 }
 
-- (void)viewWillDisappear:(BOOL)animated{
-    if (_titleTextView.text.length || _detailTextView.text.length) {
-        SheetAlertController *controller = [SheetAlertController draftsAlert];
-        [self presentViewController:controller
-                           animated:YES
-                         completion:nil];
-    }
-    else {
-        [super viewDidDisappear:YES];
-    }
-
-}
 - (void)setView{
     self.navigationController.navigationItem.title = @"求助";
     self.view.backgroundColor = [UIColor colorWithRed:228/255.0 green:228/255.0 blue:228/255.0 alpha:1.0];
@@ -254,6 +242,7 @@
 
 - (void)confirmInf{
     //textview当字数为0时返回nil  ????
+    [[[UIApplication sharedApplication] keyWindow] endEditing:YES];
     if (_titleTextView.text.length == nil||_detailTextView.text.length == nil) {
         UIAlertController *alertCon = [UIAlertController alertControllerWithTitle:@"注意" message:@"还没有完善信息哦" preferredStyle:UIAlertControllerStyleAlert];
         [alertCon addAction:[UIAlertAction actionWithTitle:@"确定"
