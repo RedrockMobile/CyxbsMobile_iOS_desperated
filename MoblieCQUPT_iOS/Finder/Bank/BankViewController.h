cb0572c61ffe380984f4d01ae71854b8dfd9181d
Ochendapao <ochendapao@icloud.com>
2018-08-17 18:33:41 +0800

    welcome2018

---
+++ MoblieCQUPT_iOS/Finder/Bank/BankViewController.h
@@ -0,0 +1,13 @@
+//
+//  BankViewController.h
+//  迎新
+//
+//  Created by 陈大炮 on 2018/8/17.
+//  Copyright © 2018年 陈大炮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BankViewController : BaseViewController
+
+@end
---
+++ MoblieCQUPT_iOS/Finder/FoodAround/FoodViewController.h
@@ -0,0 +1,13 @@
+//
+//  FoodViewController.h
+//  迎新
+//
+//  Created by 陈大炮 on 2018/8/15.
+//  Copyright © 2018年 陈大炮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FoodViewController : BaseViewController<UITableViewDelegate, UITableViewDataSource>
+
+@end
