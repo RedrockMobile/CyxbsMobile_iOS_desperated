4ee258bfc8980ac6f2301af79fde26527c10e029
LiangXiao <18016139255@163.com>
2018-08-18 12:26:42 +0800

    合并丁磊代码

---
+++ MoblieCQUPT_iOS/Finder/Freshman2018/Bus/Model/BusModel.m
@@ -0,0 +1,42 @@
+//
+//  BusModel.m
+//  MoblieCQUPT_iOS
+//
+//  Created by 丁磊 on 2018/8/14.
+//  Copyright © 2018年 Orange-W. All rights reserved.
+//
+
+#import "BusModel.h"
+
+@implementation BusModel
+
+- (instancetype)initWithDic:(NSDictionary*)dic
+{
+    self = [super init];
+    if (self) {
+        self.title = dic[@"name"];
+        self.content = dic[@"content"];
+        self.arr = dic[@"picture"];
+        self.imgArray = [self imageWithArray:self.arr];
+    }
+    return self;
+}
+
+- (NSMutableArray *)imageWithArray:(NSArray *)arr{
+    NSMutableArray *imgArr = [[NSMutableArray alloc]init];
+    imgArr = [@[] mutableCopy];
+    for (int i = 0; i < arr.count; i++) {
+        NSString *str = [NSString stringWithFormat:@"http://47.106.33.112:8080/welcome2018%@",self.arr[i]];
+        NSDate *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString: str]];
+        UIImage *img = [UIImage imageWithData:imageData];
+//        UIImage *img = [UIImage imageNamed:Arr[i]];
+        [imgArr addObject:img];
+    }
+    return imgArr;
+}
+
++ (instancetype)BusAndDeleModelWithDict:(NSDictionary *)dict{
+    return [[self alloc]initWithDic:dict];
+}
+
+@end
---
+++ MoblieCQUPT_iOS/Finder/Freshman2018/Necessity/View/IntroductionView.h
@@ -0,0 +1,16 @@
+//
+//  IntroductionView.h
+//  MoblieCQUPT_iOS
+//
+//  Created by 丁磊 on 2018/8/13.
+//  Copyright © 2018年 Orange-W. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IntroductionView : UIView
+
+@property (nonatomic, strong) UIButton *btn;
+@property (nonatomic, strong) UILabel *lab1;
+@property (nonatomic, strong) UILabel *lab2;
+@end
