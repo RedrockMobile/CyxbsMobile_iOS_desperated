b6bff1fde43435a559a763ab98ced3000412acde
LiangXiao <18016139255@163.com>
2018-08-18 12:51:23 +0800

    合并SYC代码

---
+++ MoblieCQUPT_iOS/Finder/Freshman2018/重邮风采/学生组织/Model/SYCOrganizationModel.h
@@ -0,0 +1,19 @@
+//
+//  SYCOrganizationModel.h
+//  MoblieCQUPT_iOS
+//
+//  Created by 施昱丞 on 2018/8/11.
+//  Copyright © 2018年 Orange-W. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SYCOrganizationModel : NSObject
+
+@property (nonatomic, strong)NSString *name;
+@property (nonatomic, strong)NSString *detail;
+@property (nonatomic, strong)NSMutableArray *imagesArray;
+
+- (instancetype)initWithName:(NSString *)name imageURLs:(NSArray *)imageURLs detail:(NSString *)detail;
+
+@end
---
+++ MoblieCQUPT_iOS/Finder/Freshman2018/重邮风采/学生组织/SYCOrganizationTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  SYCOrganizationTableViewController.h
+//  MoblieCQUPT_iOS
+//
+//  Created by 施昱丞 on 2018/8/16.
+//  Copyright © 2018年 Orange-W. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYCOrganizationTableViewController : UITableViewController
+
+@property NSUInteger index;
+
+@end
