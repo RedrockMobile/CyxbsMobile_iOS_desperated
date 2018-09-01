e04481a0bbad0a820c66feb7d9ebff2e19c0041c
KayDing <Kay_Ding@github.com>
2018-09-01 23:37:57 +0800

    修改回调，在viewWillDisappear里添加回调

--- MoblieCQUPT_iOS/Finder/Freshman2018/Bus/Controller/BusViewController.m
+++ MoblieCQUPT_iOS/Finder/Freshman2018/Bus/Controller/BusViewController.m
@@ -161,17 +161,6 @@
     [aView.layer addAnimation:animation forKey:nil];
 }
 
-- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer
-{
-    // 注意：只有非根控制器才有滑动返回功能，根控制器没有。
-    // 判断导航控制器是否只有一个子控制器，如果只有一个子控制器，肯定是根控制器
-    if (self.childViewControllers.count == 1) {
-        // 表示用户在根控制器界面，就不需要触发滑动手势，
-        return NO;
-    }
-    return YES;
-}
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
