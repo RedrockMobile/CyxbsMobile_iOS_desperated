6d61b4110918db4eef8f66c7274a4c1e989af346
shiyucheng1999 <774355726@qq.com>
2019-08-24 22:05:01 +0800

    修改了底栏图标

--- MoblieCQUPT_iOS/Main/MainViewController.m
+++ MoblieCQUPT_iOS/Main/MainViewController.m
@@ -19,9 +19,10 @@
 
     
     NSArray *titles = @[@"课 表",@"邮 问",@"迎 新",@"发 现",@"我 的",];
-    NSArray *images = @[@"tabbar_image_timetable",@"tabbar_image_youwen",@"",@"tabbar_image_find",@"tabbar_image_mine"];
+    NSArray *images = @[@"tabbar_image_timetable",@"tabbar_image_youwen",@"tabbar_freshman",@"tabbar_image_find",@"tabbar_image_mine"];
     self.tabBar.backgroundColor = [UIColor whiteColor];
     self.tabBar.tintColor = [UIColor colorWithHexString:@"788EFA"];
+    
     for (int i = 0; i<self.viewControllers.count; i++) {
         BaseNavigationController *nvc = self.viewControllers[i];
         nvc.topViewController.title = titles[i];
@@ -29,7 +30,14 @@
         backItem.title=@"";
         nvc.topViewController.navigationItem.backBarButtonItem = backItem;
         nvc.tabBarItem.image = [UIImage imageNamed:images[i]];
+        
+        if (i == 2) {
+            nvc.tabBarItem.selectedImage = [[UIImage imageNamed:@"tabbar_freshman"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+            nvc.tabBarItem.image = [[UIImage imageNamed:@"tabbar_freshman"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+            nvc.tabBarItem.imageInsets = UIEdgeInsetsMake(-10, 0, 10, 0);
+        }
     }
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(touchSplash:) name:@"touchSplash" object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(touchSplash:) name:@"touchSplash" object:nil];
     
     
